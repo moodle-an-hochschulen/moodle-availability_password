5c216ec4b97292ca1cdb0f94392ebf59116076a1
Alexander Bias <alexander.bias@uni-ulm.de>
2016-01-20 22:31:25 +0100

    Change copyright notes

--- db/access.php
+++ db/access.php
@@ -15,11 +15,12 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Capability definitions
+ * Availability password - Capability definitions
  *
- * @package   availability_password
- * @copyright 2016 Davo Smith, Synergy Learning
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     availabiliy
+ * @subpackage  availabiliy_password
+ * @copyright   2016 Davo Smith, Synergy Learning UK on behalf of Alexander Bias, University of Ulm <alexander.bias@uni-ulm.de>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();

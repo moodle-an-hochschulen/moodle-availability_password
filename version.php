79cc6faae7cff746c018537db7204f58895c43f9
Alexander Bias <alexander.bias@uni-ulm.de>
2021-01-10 14:30:01 +0100

    Improvement: Declare which major stable version of Moodle this plugin supports

--- version.php
+++ version.php
@@ -28,4 +28,5 @@ $plugin->component = 'availability_password';
 $plugin->version = 2020071500;
 $plugin->release = 'v3.9-r1';
 $plugin->requires = 2020061500;
+$plugin->supported = [39, 39];
 $plugin->maturity = MATURITY_STABLE;

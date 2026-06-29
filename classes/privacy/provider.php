5acf8d103c3fd89405191b488f6c251f54a32702
Alexander Bias <bias@alexanderbias.de>
2026-06-29 19:56:20 +0200

    Make codechecker happy again

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -40,8 +40,8 @@ use core_privacy\local\request\helper as request_helper;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\request\plugin\provider {
     /**
      * Returns meta data about this system.
